74faaace1f1fd7261cbf0f778e583a94727348ba
MIrzohid Mo'minov <mmuminov94@gamil.com>
2023-07-17 13:52:37 +0500

    my hometask second day's

---
+++ hometask 1/Program.cs
@@ -0,0 +1,40 @@
+﻿// data tiplarni ekranga chiqaradi
+
+var today = DateTime.Today;
+today.AddYears(today.Year);
+Console.WriteLine(today);
+
+
+var  age1 = 75;
+Console.WriteLine(age1 + " yosh");
+
+var money = 35.5;
+Console.WriteLine(money + " $");
+
+var someoneName = "Mirzohid Developer";
+Console.WriteLine(someoneName);
+
+// ismini, sharifini, yoshini kiritadi
+
+Console.WriteLine(Console.ReadLine());
+Console.WriteLine(Console.ReadLine());
+Console.WriteLine(Console.ReadLine());
+
+// Primitive va Complex tiplar uchun 
+
+int num = 12341;
+Console.WriteLine(num);
+
+double num1 = 123.456;
+Console.WriteLine(num1);
+
+DateTime date = DateTime.Now;
+Console.WriteLine(date);
+
+
+
+
+
+
+
+
---
+++ program.1/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+// Console.WriteLine("Hello, Mkxnskdfs");
+"""
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Red;
+
+while (true)
+{
+    Random random = new Random();
+    Console.Write(random.Next(10, 99) + " ");
+}
+"""
+
+// Primitive types
+
+bool hasmoney = true;
+
+char dollersign = '$';
+
+byte age = 50;
+
+sbyte normalRoomTemperature = 22;
+
+short balance = -1000;
+
+ushort distance = 60_000;
+
+int countryPopulation = 2_000_000_000;
+
+uint manufacturedItems = 4_000_000_000;
+
+long planets = 9_000_000_000_000_000_000_000;
+
+ulong planetsB = 18_000_000_000_000_000_000_000;
+
+float weight = 22.12345678F;
+
+double pi = Math.PI;
+
+Console.WriteLine(pi);
+
+

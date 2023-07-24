063c639dd8be0ad0c2ed3b59d9581a2ed8dc66d4
MIrzohid Mo'minov <mmuminov94@gamil.com>
2023-07-24 14:33:48 +0500

    https://github.com/sulton-max/BootcampN10-Level-I.git

--- Username generator/Program.cs
+++ Username generator/Program.cs
@@ -1,6 +1,6 @@
 ﻿//var substringnum = "fIRDaVs";
 //var capitalized = string.Concat(substringnum.Substring(0, 1)
-//    .ToUpper(), substringnum.Substring(1, 3).ToLower()); 
+//    .ToUpper(), substringnum.Substring(1, 3).ToLower());
 
 //Console.WriteLine(capitalized);
 
@@ -14,11 +14,31 @@
 //Console.WriteLine(input.Equals(formatted));
 
 
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.PortableExecutable;
 
 
+Console.WriteLine("Iltimos ism sharifingizni kiriting:");
+var fullName = Console.ReadLine();
+
+string[] usernames = { "mirzohidmominov", "umaralieshmurodov", "azamamonov", "azizbekabdurahmonov", "sultonbekrahimov", "shakhzodturakulov" };
+
+var username = string.Empty;
+
+for (var i = 0; i < fullName.Length; i++)
+{
+    if (fullName[i] != ' ')
+    {
+        username += fullName[i].ToString().ToLower();
+    }
+}
 
 
+var rd = new Random();
+if (usernames.Contains(username))
+{
+    username += rd.Next(1, 100);
+}
 
-
-
-
+Console.WriteLine($"Your username is: {username}");
